96d585cc4373323ef427686d0c0d28618a4326bc
tonyscruz2026-del <tonyscruz2026@gmail.com>
2026-07-31 23:36:11 +0800

    Update camera.js

--- js/hero3d/core/camera.js
+++ js/hero3d/core/camera.js
@@ -3,13 +3,26 @@ import * as THREE from "https://unpkg.com/three@0.170.0/build/three.module.js";
 export function createCamera() {
 
     const camera = new THREE.PerspectiveCamera(
+
         45,
+
         window.innerWidth / window.innerHeight,
+
         0.1,
+
         100
+
     );
 
-    camera.position.set(0, 1.5, 8);
+    camera.position.set(
+
+        0,
+
+        1.5,
+
+        8
+
+    );
 
     return camera;
 
