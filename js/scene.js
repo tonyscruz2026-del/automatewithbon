7885a054f4a7d6b7e65258886e28dbab89e1f679
tonyscruz2026-del <tonyscruz2026@gmail.com>
2026-07-31 22:44:33 +0800

    Update scene.js

--- js/scene.js
+++ js/scene.js
@@ -1,1 +1,11 @@
+import * as THREE from "https://unpkg.com/three@0.170.0/build/three.module.js";
 
+export function createScene() {
+
+    const scene = new THREE.Scene();
+
+    scene.background = null;
+
+    return scene;
+
+}
