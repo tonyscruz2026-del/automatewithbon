f7bf201fbbc1543893f97584dcd1a5d3bf4307c3
tonyscruz2026-del <tonyscruz2026@gmail.com>
2026-08-01 00:53:25 +0800

    Update scene.js

--- js/hero3d/core/scene.js
+++ js/hero3d/core/scene.js
@@ -4,7 +4,12 @@ export function createScene() {
 
     const scene = new THREE.Scene();
 
-    scene.background = null;
+    scene.background = new THREE.Color(0x050816);
+
+    scene.fog = new THREE.FogExp2(
+        0x050816,
+        0.03
+    );
 
     return scene;
 
